5561a1d58a842d6aa6b8f7fae0bd08507e791ddc
lalabohm <lalala.bohm@gmail.com>
2025-06-28 11:06:11 -0300

    Create chefe.c

---
+++ src/chefe.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "structs.h"
+
+extern Pedido *inicio extern pthread_mutex_t mutexPedidos;
+extern pthread_mutex_t mutexBancadas;
+extern int muralAtivo;
+
+void *chefeDeCozinha(void *arg)
+{
+    Tripulante *tripulantes = (Tripulante *)arg;
+
+    while (muralAtivo)
+    {
+        pthread_mutex_lock(&mutexPedidos);
+        if (inicio == NULL)
+        {
+            pthread_mutex_unlock(&mutexPedidos);
+            sleep(1);
+            continue;
+        }
+
+        Pedido *pedido = inicio;
+
+        int tripulanteLivre = -1;
+        for (int i = 0; i < 4; i++)
+        {
+            if (tripulantes[i].ocupapdo == 0)
+            {
+                tripulanteLivre = i;
+                break;
+            }
+        }
+
+        if (tripulanteLivre == -1)
+        {
+            pthread_mutex_unlock(&mutexPedidos);
+            sleep(1);
+            continue;
+        }
+
+        inicio = inicio->proximo;
+        tripulantes[tripulanteLivre].pedidoAtual = pedido;
+        tripulantes[tripulanteLivre].ocupado = 1;
+
+        printf("O chefe atribuiu o prato %s para Tripulante %d\n", pedido->nome, tripulantes[tripulanteLivre].id);
+
+        pthread_mutex_unlock(&mutexPedidos);
+
+        sleep(1)
+    }
+
+    return NULL;
+}
